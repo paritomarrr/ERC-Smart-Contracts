35aaa77fe386f200c7d65b03f863cef6308b62c9
paritomarrr <pari2019cse083abesit@gmail.com>
2025-05-05 12:19:32 +0530

    Added: New folder structure

--- old/hardhat/contracts/mocks/ERC20Mock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import {ERC20} from "../token/ERC20.sol";
-
-contract ERC20Mock is ERC20 {
-    constructor() ERC20("ERC20Mock", "E20M", 18) {}
-
-}
